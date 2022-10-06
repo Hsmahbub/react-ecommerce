799275956a3812f7e98ff1e4f58811e93fe50ed3
Md Mahbub Alom <87687925+mahbub-dev@users.noreply.github.com>
2022-10-06 10:51:19 +0600

    Update requestMethod.js

--- src/utils/requestMethod.js
+++ src/utils/requestMethod.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const local__server = "http://localhost:4000/api";
 const cloud__server = "https://mahbub-commerce.herokuapp.com/api";
-const BASE_URL = local__server;
+const BASE_URL = cloud__server;
 const userToken = localStorage.getItem("userToken");
 const adminToken = localStorage.getItem("adminToken");
 export const publicRequest = axios.create({
