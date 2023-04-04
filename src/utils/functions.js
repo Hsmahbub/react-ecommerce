61cbcd094be14ba6c13a726f16edf6df8e81faec
Hsmahbub <hsmahbub@gmail.com>
2023-04-04 12:26:30 +0600

    navbar position setting sticky

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -8,4 +8,25 @@
 	return values;
 };
 
+// Add a scroll event listener to the window
+window.addEventListener("scroll", () => {
+	const stickyElement = document.querySelector(".navbar");
+	const searchElement = document.querySelector(".search-input-field");
+	console.log(stickyElement);
+	const stickyPosition = stickyElement?.offsetTop;
+	const searchElementPosition = stickyElement?.offsetTop;
+	// Get the current scroll position
+	const scrollPosition = window.scrollY;
+
+	// Check if the scroll position has passed the sticky element
+	if (scrollPosition >= stickyPosition) {
+		// Add the "sticky" CSS class to the element
+		stickyElement.classList.add("sticky");
+	}
+	if (scrollPosition <= searchElementPosition) {
+		// Remove the "sticky" CSS class from the element
+		stickyElement.classList.remove("sticky");
+	}
+});
+
 export { customIterator };
